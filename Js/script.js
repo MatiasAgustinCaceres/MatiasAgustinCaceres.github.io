bed8269123b8cb1ac27edb9f85d76146721e86e1
MatiasCaceres <matiasagustincaceres@hotmail.com>
2022-06-03 00:03:12 -0300

    Script bug solved

--- Js/script.js
+++ Js/script.js
@@ -16,11 +16,12 @@ const nav = document.querySelector(".nav"),
     for(let i=0; i<totalNavList; i++) {
         const a = navList[i].querySelector("a");
         a.addEventListener("click", function() {
-            removeBackSection();
+            for(let i=0; i<totalSection; i++){
+                allSection[i].classList.remove("back-section");
+            }
             for(let j=0; j<totalNavList; j++) {
                 if(navList[j].querySelector("a").classList.contains("active")) {
-                    addBackSection(j);
-                    //allSection[j].classList.add("back-section");
+                    allSection[j].classList.add("back-section");
                 }
                 navList[j].querySelector("a").classList.remove("active");
             }
@@ -31,14 +32,6 @@ const nav = document.querySelector(".nav"),
             }
         })
     }
-    function removeBackSection (){
-        for(let i=0; i<totalSection; i++){
-            allSection[i].classList.remove("back-section");
-        }
-    }
-    function addBackSection (num){
-        allSection[num].classList.add("back-section");
-    }
     function showSection(element) {
         for(let i=0; i<totalSection; i++){
             allSection[i].classList.remove("active");
@@ -46,22 +39,6 @@ const nav = document.querySelector(".nav"),
         const target = element.getAttribute("href").split("#")[1];
         document.querySelector("#" + target).classList.add("active")
     }
-    function updateNav(element){
-        for(let i=0; i<totalNavList; i++) {
-            navList[i].querySelector("a").classList.remove("active");
-            const target = element.getAttribute("href").split("#")[1];
-            if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1]) {
-                navList[i].querySelector("a").classList.add("active");
-            }
-        }
-    }
-    doocument.querySelector(".hire-me").addEventListener("click", function() {
-        const setionIndex = this.getAttribute("data-section-index");
-        showSection(this);
-        updateNav(this);
-        removeBackSection();
-        addBackSection(setionIndex);
-    })
     const navTogglerBtn = document.querySelector(".nav-toggler"),
         aside = document.querySelector(".aside");
         navTogglerBtn.addEventListener("click", () => {
